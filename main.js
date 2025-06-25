49e79c43848bf91fc33296fce673a29629535edf
Edu-Dev2024 <eduwel_developer@outlook.com>
2025-06-25 15:05:31 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { Game } from './game.js';
-
-// Initialize the game
-const game = new Game();
-window.game = game; // Make globally accessible for restart button
-
-// Start the game
-game.start();
